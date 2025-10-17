023f2a1c1f053696da7f89c7b372a7b207b79b1c
plan <frostyplanet@gmail.com>
2025-10-17 08:21:55 +0800

    error temp

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,6 +1,4 @@
 use crate::Codec;
-use serde::{Deserialize, Serialize};
-use std::convert::{TryFrom, TryInto};
 use std::fmt;
 
 pub const RPC_ERR_PREFIX: &'static str = "rpc_";
@@ -8,13 +6,23 @@ pub const RPC_ERR_PREFIX: &'static str = "rpc_";
 /// A error type defined by client-side user logic
 ///
 /// Due to possible decode
-#[derive(thiserror::Error)]
+#[derive(thiserror::Error, Debug)]
 pub enum RpcError<E: RpcErrCodec> {
     User(E),
     Rpc(RpcIntErr),
 }
+impl<E: RpcErrCodec> fmt::Display for RpcError<E> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::User(e) => fmt::Display::fmt(e, f),
+            Self::Rpc(e) => fmt::Display::fmt(e, f),
+        }
+    }
+}
 
 impl<E: RpcErrCodec> std::cmp::PartialEq<RpcIntErr> for RpcError<E> {
+    #[inline]
     fn eq(&self, other: &RpcIntErr) -> bool {
         if let Self::Rpc(r) = self {
             if r == other {
@@ -26,6 +34,7 @@ impl<E: RpcErrCodec> std::cmp::PartialEq<RpcIntErr> for RpcError<E> {
 }
 
 impl<E: RpcErrCodec + PartialEq> std::cmp::PartialEq<RpcError<E>> for RpcError<E> {
+    #[inline]
     fn eq(&self, other: &Self) -> bool {
         match self {
             Self::Rpc(r) => {
@@ -40,6 +49,20 @@ impl<E: RpcErrCodec + PartialEq> std::cmp::PartialEq<RpcError<E>> for RpcError<E
             }
         }
         false
+    }
+}
+
+impl<E: RpcErrCodec> From<E> for RpcError<E> {
+    #[inline]
+    fn from(e: E) -> Self {
+        Self::User(e)
+    }
+}
+
+impl From<&str> for RpcError<String> {
+    #[inline]
+    fn from(e: &str) -> Self {
+        Self::User(e.to_string())
     }
 }
 
@@ -98,7 +121,7 @@ impl<E: RpcErrCodec> From<RpcIntErr> for RpcError<E> {
 ///     }
 /// }
 /// ```
-pub trait RpcErrCodec: Send + Sized + 'static + Unpin {
+pub trait RpcErrCodec: Send + Sized + 'static + Unpin + fmt::Debug + fmt::Display {
     fn encode<C: Codec>(&self, codec: &C) -> EncodedErr;
 
     fn decode<C: Codec>(codec: &C, buf: Result<u32, &[u8]>) -> Result<Self, ()>;
@@ -142,7 +165,7 @@ impl RpcErrCodec for nix::errno::Errno {
     fn decode<C: Codec>(_codec: &C, buf: Result<u32, &[u8]>) -> Result<Self, ()> {
         if let Ok(i) = buf {
             if i <= i32::max as u32 {
-                return Ok(Self::from_i32(i as i32));
+                return Ok(Self::from_raw(i as i32));
             }
         }
         Err(())
@@ -155,7 +178,7 @@ impl RpcErrCodec for String {
         EncodedErr::Buf(Vec::from(self.as_bytes()))
     }
     #[inline(always)]
-    fn decode<C: Codec>(codec: &C, buf: Result<u32, &[u8]>) -> Result<Self, ()> {
+    fn decode<C: Codec>(_codec: &C, buf: Result<u32, &[u8]>) -> Result<Self, ()> {
         if let Err(s) = buf {
             if let Ok(s) = str::from_utf8(s) {
                 return Ok(s.to_string());
@@ -167,7 +190,7 @@ impl RpcErrCodec for String {
 
 /// "rpc_" prefix is reserved for internal error
 #[derive(
-    strum::Display, strum::EnumString, strum::AsRefStr, Debug, PartialEq, Clone, thiserror::Error,
+    strum::Display, strum::EnumString, strum::AsRefStr, PartialEq, Clone, thiserror::Error,
 )]
 #[repr(u8)]
 pub enum RpcIntErr {
@@ -200,6 +223,14 @@ pub enum RpcIntErr {
     Version,
 }
 
+// The default Debug derive just ignore strum customized string, by strum only have a Display derive
+impl fmt::Debug for RpcIntErr {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
+}
+
 impl RpcIntErr {
     #[inline]
     pub fn as_bytes<'a>(&'a self) -> &'a [u8] {
@@ -209,7 +240,7 @@ impl RpcIntErr {
 
 impl From<std::io::Error> for RpcIntErr {
     #[inline(always)]
-    fn from(e: std::io::Error) -> Self {
+    fn from(_e: std::io::Error) -> Self {
         Self::IO
     }
 }
@@ -311,80 +342,55 @@ impl fmt::Display for EncodedErr {
     }
 }
 
-///// the same with errno
-//impl From<i32> for EncodedErr {
-//    #[inline(always)]
-//    fn from(e: i32) -> Self {
-//        EncodedErr::Num(e)
-//    }
-//}
-//
-//impl From<&'static str> for EncodedErr {
-//    #[inline(always)]
-//    fn from(s: &'static str) -> Self {
-//        EncodedErr::Str(s)
-//    }
-//}
-//
-//impl From<&String> for EncodedErr {
-//    #[inline(always)]
-//    fn from(s: &String) -> Self {
-//        EncodedErr::Text(s.to_string())
-//    }
-//}
-//
-//impl From<String> for EncodedErr {
-//    #[inline(always)]
-//    fn from(s: String) -> Self {
-//        EncodedErr::Text(s)
-//    }
-//}
-//
-///// For nix::errno
-//impl Into<i32> for EncodedErr {
-//    #[inline(always)]
-//    fn into(self) -> i32 {
-//        if let EncodedErr::Num(i) = self {
-//            return i as i32;
-//        } else {
-//            unimplemented!();
-//        }
-//    }
-//}
-
 impl From<RpcIntErr> for EncodedErr {
     #[inline(always)]
     fn from(e: RpcIntErr) -> Self {
         Self::Rpc(e)
     }
 }
-//
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//    use nix::errno::Errno;
-//    use std::str::FromStr;
-//
-//    #[test]
-//    fn test_error_conversion() {
-//        println!("{}", RpcIntErr::Internal);
-//        println!("{:?}", RpcIntErr::Internal);
-//        let s = RpcIntErr::Timeout.as_ref();
-//        println!("RpcIntErr::Timeout as {}", s);
-//        let e = RpcIntErr::from_str(s).expect("parse");
-//        assert_eq!(e, RpcIntErr::Timeout);
-//        assert!(RpcIntErr::from_str("timeoutss").is_err());
-//
-//        println!("test errno {:?}", Errno::EIO);
-//        let e: EncodedErr = Errno::EIO.into();
-//        println!("{}", e);
-//        println!("{:?}", e);
-//
-//        println!("test from str");
-//        let e = EncodedErr::from("err_str");
-//        println!("{:?}", e);
-//
-//        let e = EncodedErr::from("err_str".to_string());
-//        println!("{:?}", e);
-//    }
-//}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nix::errno::Errno;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_internal_error() {
+        println!("{}", RpcIntErr::Internal);
+        println!("{:?}", RpcIntErr::Internal);
+        let s = RpcIntErr::Timeout.as_ref();
+        println!("RpcIntErr::Timeout as {}", s);
+        let e = RpcIntErr::from_str(s).expect("parse");
+        assert_eq!(e, RpcIntErr::Timeout);
+        assert!(RpcIntErr::from_str("timeoutss").is_err());
+    }
+
+    #[test]
+    fn test_rpc_error_default() {
+        let e = RpcError::<i32>::from(1i32);
+        println!("err {:?} {}", e, e);
+
+        let e = RpcError::<Errno>::from(Errno::EIO);
+        println!("err {:?} {}", e, e);
+
+        let e = RpcError::<String>::from("err_str");
+        println!("err {:?} {}", e, e);
+
+        let e = RpcError::<String>::from(RpcIntErr::IO);
+        println!("err {:?} {}", e, e);
+    }
+
+    //#[test]
+    //fn test_rpc_error_string_enum() {
+    //    not supported by default, should provide a derive
+    //    #[derive(
+    //        Debug, strum::Display, strum::EnumString, strum::AsRefStr, PartialEq, Clone, thiserror::Error,
+    //    )]
+    //    enum MyError {
+    //        OhMyGod,
+    //    }
+    //    let e = RpcError::<MyError>::from(MyError::OhMyGod);
+    //    println!("err {:?} {}", e, e);
+    //}
+}
