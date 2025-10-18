293c59424bc241d221e0ad56de5446459412c1e6
plan <frostyplanet@gmail.com>
2025-10-18 14:33:02 +0800

    change encode interface
    core: Codec add encode_into() with specified buffer. This is better for
    pre-encodec bytes message without the need to alloc owned Vec<u8>
    
    stream: Change ClientTaskEncode to ouput to io buffer (For client task from API interface)
    
    Upgrade zero-copy version to 0.8, refactor proto::ReqHead RespHead usage
    (with Codecc::encode_into())
    
    Remove zerocopy dep from transport/tcp

--- core/src/codec.rs
+++ core/src/codec.rs
@@ -1,5 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+/*
+ *  Note that there's no unify output interface in each serde impl,
+ *  whatever we want to serialize into (std::io::Write / Buffer/ Vec<u8>),
+ *  require the codec implment to match.
+ */
+
 /// Interface for [occams-rpc-codec](https://docs.rs/occams-rpc-codec)
 ///
 /// The codec is immutable, if need changing (like setting up cipher), should have inner
@@ -7,16 +13,18 @@ use serde::{Deserialize, Serialize};
 pub trait Codec: Default + Send + Sync + Sized + 'static {
     fn encode<T: Serialize>(&self, task: &T) -> Result<Vec<u8>, ()>;
 
+    /// sererialized the msg into buf (with std::io::Writer), and return the size written
+    fn encode_into<T: Serialize>(&self, task: &T, buf: &mut Vec<u8>) -> Result<usize, ()>;
+
     fn decode<'a, T: Deserialize<'a>>(&self, buf: &'a [u8]) -> Result<T, ()>;
 }
 
+/*
 #[allow(dead_code)]
 pub trait BytesCodec: Codec {
-    fn encode<T: Serialize>(&self, task: &T) -> Result<Vec<u8>, ()>
-    where
-        T: AsRef<u8>;
 
-    fn decode<'a, T: Deserialize<'a>>(&self, buf: &'a [u8]) -> Result<T, ()>
-    where
-        T: AsRef<u8>;
+    fn encode<T: Serialize>(&self, task: &T) -> Result<Vec<u8>, ()> where T: AsRef<u8>;
+
+    fn decode<'a, T: Deserialize<'a>>(&self, buf: &'a [u8]) -> Result<T, ()> where T: AsRef<u8>;
 }
+*/
